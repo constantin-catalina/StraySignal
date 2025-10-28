961ffeb54daa2108cb7a8e4877339e8a5d01432a
Constantin Catalina <catalina.constantin@student.upt.ro>
2025-10-28 22:28:35 +0200

    initial commit

---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,8 @@
+import { SafeAreaView, Text, View } from "react-native";
+export default function Screen() {
+  return (
+    <SafeAreaView style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <View><Text>Coming soon…</Text></View>
+    </SafeAreaView>
+  );
+}
